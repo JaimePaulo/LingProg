3268b3461a3bc145226cd9265a45e6d37394d315
Jaime Paulo <jmfpaulo@outlook.pt>
2024-11-12 09:21:36 +0000

    Add files via upload
    Programa para Calcular a Área e o perímetro de um quadrado

---
+++ AreaPerimetro.py
@@ -0,0 +1,15 @@
+# Problema - Calcular a area e o perimetro de um 
+#           quadrado ou retangulo
+
+# Ler o tamanho do lado do quadrado/Retangulo
+lado = int(input("Qual o tamanho do lado em m?"))
+
+# Calcular a area do quadrado
+area = lado * lado
+
+# Calcular o Perimetro do Quadrado
+perimetro = 4 * lado
+
+# Apresentar o resultado
+print("Area =", area)
+print("Perimetro =", perimetro)
